53cb67301edbc2c54015409f8486d1837c552f3f
MariaRomanDelonia <maria.roman@delonia.es>
2020-03-17 12:12:49 +0100

    quitamos un .then innecesario

--- script.js
+++ script.js
@@ -3,18 +3,18 @@ function fetchChuckJSON() {
 
     const url = `https://api.chucknorris.io/jokes/random`;
     axios.get(url)
-        .then(response => response.data) // DIFFERENT FROM FETCH: response.data instead of response.json()
-        .then(chuck => {
+        .then(response => { // DIFFERENT FROM FETCH: response.data instead of response.json()
             {
-                console.log('data decoded from JSON:', chuck);
+                console.log('data decoded from JSON:', response.data);
                 // Build a block of HTML
                 const chuckHtml = `
-                <img src = "${chuck.icon_url}"/>
-                <p><strong>${chuck.value}</strong></p>
+                <img src = "${response.data.icon_url}"/>
+                <p><strong>${response.data.value}</strong></p>
                 `;
                 document.querySelector('#chuck-norris').innerHTML = chuckHtml;
             }
         });
+        
 }
 document.getElementById("myBtn").addEventListener("click", function () {
     fetchChuckJSON();
